eebdde82a31cd1119fc7248e60b4fb6d7c766df5
Alexandre Dupierris <dupierris.alexandre@gmail.com>
2025-03-17 10:50:41 +0100

    sharp to the middleware from the controler

--- controllers/books.js
+++ controllers/books.js
@@ -1,20 +1,21 @@
 const Book  = require('../models/Book');
-const path = require('path');
-const sharp = require('sharp');
 const fs = require('fs');
 
+// getting the books list
 exports.getAllBooks = (req, res, next) => {
     Book.find()
         .then(books => res.status(200).json(books))
         .catch(error => res.status(400).json({error}));
 };
 
+// getting one book with the id from the dynamic url
 exports.getOneBook = (req, res, next) => {
     Book.findOne({_id: req.params.id})
         .then(book => res.status(200).json(book))
         .catch(error => res.status(404).json({error}));
 };
 
+// creating a new book
 exports.createBook = (req, res, next) => {
     const bookObject = JSON.parse(req.body.book);
     delete bookObject._id;
@@ -22,87 +23,82 @@ exports.createBook = (req, res, next) => {
     const rating = bookObject.rating;
     let book;
 
+    // if there is no image
     if (!req.file) {
         return res.status(400).json({ message: "Aucune image envoyée" });
     }
-    const inputPath = req.file.path;
-    const outputFilename = `${req.file.filename.split('.')[0]}.webp`;
-    const outputPath = path.join('images', outputFilename);
-
-    sharp(inputPath)
-        .resize({ width: 500 })
-        .toFormat('webp')
-        .webp({ quality: 80 })
-        .toFile(outputPath)
-        .then(() => {
-            fs.unlinkSync(inputPath);
-            if (rating){
-                if (rating < 0 || rating > 5) {
-                    return res.status(400).json({ message: 'La note doit être comprise entre 0 et 5'});
-                }
-                book = new Book({
-                    ...bookObject,
-                    userId: req.auth.userId,
-                    imageUrl: `${req.protocol}://${req.get('host')}/images/${outputFilename}`,
-                    ratings: [{ userId: req.auth.userId, grade: rating }],
-                    averageRating: rating
-                });
-            }
-            else {
-                book = new Book({
-                    ...bookObject,
-                    userId: req.auth.userId,
-                    imageUrl: `${req.protocol}://${req.get('host')}/images/${outputFilename}`,
-                    ratings: [],
-                    averageRating: 0
-                });
-            }
-            book.save()
-                .then(() => res.status(201).json({message: 'Livre enregistré'}))
-                .catch(error => res.status(400).json({error}));
-        })
-        .catch(error => res.status(500).json({ error }));
+    // if rating is defined
+    if (rating) {
+        if (rating < 0 || rating > 5) {
+            return res.status(400).json({ message: 'La note doit être comprise entre 0 et 5'});
+        }
+        book = new Book({
+            ...bookObject,
+            userId: req.auth.userId,
+            imageUrl: req.file.processedImageUrl,
+            ratings: [{ userId: req.auth.userId, grade: rating }],
+            averageRating: rating
+        });
+    // else if the rating is not defined
+    } else {
+        book = new Book({
+            ...bookObject,
+            userId: req.auth.userId,
+            imageUrl: req.file.processedImageUrl,
+            ratings: [],
+            averageRating: 0
+        });
+    }
+    // registering the new book
+    book.save()
+        .then(() => res.status(201).json({message: 'Livre enregistré'}))
+        .catch(error => res.status(400).json({error}));
 };
 
+// modifying an existing book
 exports.modifyBook = (req, res, next) => {
-    const inputPath = req.file.path;
-    const outputFilename = `${req.file.filename.split('.')[0]}.webp`;
-    const outputPath = path.join('images', outputFilename);
-
-    sharp(inputPath)
-        .resize({ width: 500 })
-        .toFormat('webp')
-        .webp({ quality: 80 })
-        .toFile(outputPath)
-        .then(() => {
-            fs.unlinkSync(inputPath);
-            const bookObject = req.file ? {
-                ...JSON.parse(req.body.book),
-                imageUrl: `${req.protocol}://${req.get('host')}/images/${outputFilename}`
-            } : {...req.body};
-            delete bookObject._userId;
-            Book.findOne({_id: req.params.id})
-            .then((book) => {
-                if (book.userId != req.auth.userId) {
-                    res.status(403).json({message: 'Non-autorisé'});
+    // if there is an image, get json from the object book from the req.body, else, get the req.body
+    const bookObject = req.file ? {
+        ...JSON.parse(req.body.book),
+        imageUrl: req.file.processedImageUrl
+    } : {...req.body};
+    
+    delete bookObject._userId;
+    // finding the book from the dynamic url
+    Book.findOne({_id: req.params.id})
+        .then((book) => {
+            // if it's not the user that have created the book who wants to edit it
+            if (book.userId != req.auth.userId) {
+                return res.status(403).json({message: 'Non-autorisé'});
+            }
+            
+            // if a new image is upload, delete the last one
+            if (req.file) {
+                const filename = book.imageUrl.split('/images/')[1];
+                try {
+                    fs.unlinkSync(`images/${filename}`);
+                } catch (error) {
+                    console.error('Erreur lors de la suppression de l\'ancienne image:', error);
                 }
-                else {
-                    Book.updateOne({_id: req.params.id}, {...bookObject, _id: req.params.id})
-                    .then(() => res.status(200).json({message: 'Livre modifié'}))
-                    .catch(error => {res.status(401).json(error)});
-                }
-            })
-            .catch(error => {res.status(400).json({error})});
+            }
+            // updating the book
+            Book.updateOne({_id: req.params.id}, {...bookObject, _id: req.params.id})
+                .then(() => res.status(200).json({message: 'Livre modifié'}))
+                .catch(error => res.status(401).json(error));
         })
-        .catch(error => res.status(500).json({ error }));
+        .catch(error => res.status(400).json({error}));
 };
 
+// deleting a book
 exports.deleteBook = (req, res, next) => {
+    // finding the book with the id from the url
     Book.findOne({_id: req.params.id})
     .then(book => {
+        // if user is not authorised
         if(book.userId != req.auth.userId) {
             res.status(403).json({message: 'Non-autorisé'});
         }
+        // else, deleting the image file from the folder, and the book from the bdd
         else {
             const filename = book.imageUrl.split('/images/')[1];
             fs.unlink(`images/${filename}`, () => {
@@ -115,6 +111,7 @@ exports.deleteBook = (req, res, next) => {
     .catch(error => {res.status(500).json({error})});
 };
 
+// creating rating
 exports.createRating = (req, res, next) => {
     const bookObject = req.body;
     delete bookObject._id;
@@ -125,23 +122,28 @@ exports.createRating = (req, res, next) => {
     if (rating < 0 || rating > 5) {
         return res.status(400).json({message: 'La note doit être comprise entre 0 et 5'});
     }
-
+    // finding the book from url id
     Book.findById(bookId)
     .then(book => {
+        // if book doesn't exist
         if(!book){
             return res.status(404).json({message: 'Livre non trouvé'});
         }
         const alreadyRated = book.ratings.find(ratin => ratin.userId === userId);
+        // if the book is already rated by this user
         if (alreadyRated) {
             return res.status(400).json({message: 'Vous avez déjà noté ce livre'});
         }
+        // pushing the book rating to the ratings
         book.ratings.push({userId: userId, grade: rating});
         const totalRatings = book.ratings.length;
+        // calculating the average rating
         let sumRatings = 0;
         for (let ratin of book.ratings) {
             sumRatings += ratin.grade;
         }
         book.averageRating = sumRatings / totalRatings;
+        // saving the book to the bdd
         book.save()
             .then(() => res.status(201).json(book))
             .catch(error => res.status(400).json({error}));
@@ -149,7 +151,9 @@ exports.createRating = (req, res, next) => {
     .catch(error => res.status(500).json({ error }));
 };
 
+// getting best books
 exports.getBestBooks = (req, res, next) => {
+    // sorting by average ratings, the three best books
     Book.find().sort({averageRating: -1}).limit(3)
         .then(books => {
             res.status(200).json(books)
--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,4 +1,7 @@
 const multer = require('multer');
+const path = require('path');
+const sharp = require('sharp');
+const fs = require('fs');
 
 const MIME_TYPES = {
   'image/jpg': 'jpg',
@@ -6,9 +9,10 @@ const MIME_TYPES = {
   'image/png': 'png',
 };
 
+// config of the temp storage for multer
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, 'images');
+    callback(null, 'images/temp');
   },
   filename: (req, file, callback) => {
     const name = file.originalname.split(' ').join('_');
@@ -17,4 +21,71 @@ const storage = multer.diskStorage({
   }
 });
 
-module.exports = multer({storage: storage}).single('image');
+// create the temp folder if doesn't exist
+const tempDir = path.join('images', 'temp');
+if (!fs.existsSync('images')) {
+  fs.mkdirSync('images');
+}
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir);
+}
+
+// image format
+const fileFilter = (req, file, callback) => {
+  if (Object.keys(MIME_TYPES).includes(file.mimetype)) {
+    callback(null, true);
+  } else {
+    callback(new Error('Format d\'image non supporté'), false);
+  }
+};
+
+
+// Middleware to treat the image after the upload
+const processImage = async (req, res, next) => {
+  if (!req.file) {
+    return next();
+  }
+  
+  try {
+    const inputPath = req.file.path;
+    const outputFilename = `${req.file.filename.split('.')[0]}.webp`;
+    const outputPath = path.join('images', outputFilename);
+
+    await sharp(inputPath)
+      .resize({ width: 500 })
+      .toFormat('webp')
+      .webp({ quality: 80 })
+      .toFile(outputPath);
+    
+    // delete the temp file
+    fs.unlinkSync(inputPath);
+    
+    // update the path in the object req.file
+    req.file.filename = outputFilename;
+    req.file.path = outputPath;
+    req.file.destination = 'images';
+    req.file.mimetype = 'image/webp';
+    req.file.processedImageUrl = `${req.protocol}://${req.get('host')}/images/${outputFilename}`;
+    
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Export a combined middleware
+module.exports = (req, res, next) => {
+  const upload = multer({ storage, fileFilter }).single('image');
+  
+  upload(req, res, (err) => {
+    if (err) {
+      return res.status(400).json({ message: `Erreur lors de l'upload: ${err.message}` });
+    }
+    
+    if (!req.file) {
+      return next();
+    }
+    
+    processImage(req, res, next);
+  });
+};
